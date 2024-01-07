2d4314e85bdf6faab79a3718d6fb4ec8635679ad
Aphilak Jankaewdet <aphilakjankaewdach@gmail.com>
2024-01-07 19:00:34 +0700

    fix calculator& C:/Users/Xhier/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Xhier/Desktop/AphilakTest/kivy/lab3/LAB3-4/cal.py

--- lab3/LAB3-4/cal.py
+++ lab3/LAB3-4/cal.py
@@ -1,15 +1,16 @@
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class Calculator(BoxLayout):
     label = ''
- 
+    
     def delete(self, instance):
         self.display.text = instance[:0]
- 
+    
     def del1(self, instance):
         self.display.text = instance[:-1]
- 
+    
     def calc(self, instance):
         try:
             self.display.text = str(eval(instance))
@@ -17,14 +18,14 @@ class Calculator(BoxLayout):
         except Exception:
             self.display.text = '0'
             self.result.text = 'ERROR'
- 
+        
 class CalculatorApp(App):
     trigger = False
     triggerC = False
     triggerD = False
- 
+    
     def build(self):
         return Calculator()
- 
+        
 if __name__ == '__main__':
     CalculatorApp().run()
